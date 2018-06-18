2e9ad108e46e00970415cc9abc10e0c8cd4c47ef
Guus Klok <guus.klok@gmail.com>
2018-06-18 13:48:24 +0200

    first commit

---
+++ textadventure_guus_klok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace textadventure_guus_klok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
